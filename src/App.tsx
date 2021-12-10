1c02cab6dd589e3c3665f1f58701373ef13f33c9
Sergejs-Rudko <sergejs.rudko@gmail.com>
2021-12-10 16:09:08 +0000

    check ?

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { Routing } from "./components/Routes";
 import { Header } from "./components/Header/Header";
-
+// testing push to GH
 export const App = () => {
     return (
         <div className="App">
