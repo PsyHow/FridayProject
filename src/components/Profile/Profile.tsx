373bebb3598c01b53ebeefd0a607b387edab72c9
PsyHow <viktorburnyshev@gmail.com>
2021-12-17 23:17:45 +0600

    fix redirect

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,28 +2,26 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { AppRootStoreType } from "../../bll/Store";
 import { UserType } from "../../bll/profileReducer";
+import { Navigate } from "react-router-dom";
 
 export const Profile = () => {
 
     let user = useSelector<AppRootStoreType, UserType>(state => state.profileReducer.user)
-    const loggedIn = useSelector<AppRootStoreType,boolean>(state=> state.loginReducer.isLogged)
+    const loggedIn = useSelector<AppRootStoreType, boolean>(state => state.loginReducer.isLogged)
 
     const style = {
         width: "200px",
-        height: "200px"
+        height: "200px",
     }
     return (
         <div>
             {
-                !loggedIn
-                    ? <span>You need to login first</span>
+                !loggedIn ? <Navigate to="/login"/>
                     : <>
-                        <span>{user.name}</span>
-                        <img style={style} src={user.avatar} alt="user avatar"/>
+                        <span>{ user.name }</span>
+                        <img style={ style } src={ user.avatar } alt="user avatar"/>
                     </>
             }
-
-
         </div>
     )
 }
