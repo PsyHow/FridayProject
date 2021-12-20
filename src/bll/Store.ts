71a924f386312268520dc8a0fce76e5f55f3a4b7
PsyHow <viktorburnyshev@gmail.com>
2021-12-20 19:09:36 +0600

    fix

--- src/bll/Store.ts
+++ src/bll/Store.ts
@@ -5,17 +5,12 @@ import { loginReducer } from "./loginReducer";
 import { profileReducer } from "./profileReducer";
 import { passwordRecoverReducer } from "./passwordRecoverReducer";
 import { signUpReducer } from "./signUpReducer";
-<<<<<<<<< Temporary merge branch 1
-import { pageReducer } from "./paginator_reducer/paginator_reducer";
-=========
 import {CardPacksActionsType, cardPacksReducer} from "../components/Table/CardPacks/CardPacksReducer";
->>>>>>>>> Temporary merge branch 2
 
 const reducers = combineReducers({
     firstReducer,
     loginReducer,
     profileReducer,
-    pageReducer,
     recovery: passwordRecoverReducer,
     registration: signUpReducer,
     cards: cardPacksReducer
